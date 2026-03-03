485d94d068eab8451bbeee4be6509f7577489cbd
Surya816 <133378550+Surya816@users.noreply.github.com>
2026-03-03 08:29:06 +0530

    Update service worker for new cache version and assets

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-const CACHE = 'battle-grid-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch',   e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'battle-grid-v2';
+const ASSETS = [
+    '/battle-grid/',
+    '/battle-grid/new.html',
+    '/battle-grid/manifest.json',
+    '/battle-grid/icon-192.png',
+    '/battle-grid/icon-512.png'
+];
+self.addEventListener('install', e => e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+));
+self.addEventListener('activate', e => e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+));
+self.addEventListener('fetch', e => e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+));
